247849b4368d8b1958956fe52825216f8debcc98
alper <basdoganalper@gmail.com>
2022-08-19 15:18:29 +0300

    second commit

--- YTapp/urls.py
+++ YTapp/urls.py
@@ -1,9 +1,12 @@
 #from django.contrib import admin
 from django.urls import path
 from YTapp import views
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
     #path('admin/', admin.site.urls),
     path('download/', views.download_video, name='download'),
 ]
--- YTapp/views.py
+++ YTapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-import youtube_dl
+import yt_dlp
 from django.contrib import messages
+from django.views.generic import TemplateView
+
 # Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -12,8 +22,12 @@ def download_video(request):
     if request.method == 'POST':
         video_url = request.POST['url']
         if video_url:
-            ydl_opts = {'outtmp1': 'D:/'}
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl_opts = {
+                "format": "mp4/res:1080",
+                'outtmp1': 'D:/',
+                'ignoreerrors:': 'only_download',
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([video_url])
             messages.success(request, 'Video Downloaded.')
             return redirect('home')
